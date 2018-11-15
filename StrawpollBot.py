944d5d2b4621411dd2f2c2fb1309f23527cda68a
Arilis <32502295+Arilis@users.noreply.github.com>
2018-11-15 10:33:17 -0500

    Update StrawpollBot.py

--- StrawpollBot.py
+++ StrawpollBot.py
@@ -1,10 +1,35 @@
 import requests
 import string
-strawpollID = "ID HERE"
-url = 'https://www.strawpoll.me/' + strawpollID
+import time
+import sys
+import colorful
+import os
+
+clear = lambda: os.system('cls')
+strawid = input("What is the poll ID located in the poll URL?: ")
+opid = input("What is the option ID located in the F12 menu?: ")
+
+print("Strawpoll ID Set to: " + colorful.green(strawid))
+time.sleep(2)
+print("Strawpoll Option ID Set to: " + colorful.green(opid))
+time.sleep(2)
+
+url = 'https://www.strawpoll.me/' + strawid
+print(colorful.red("Starting bot in 5 seconds!"))
+time.sleep(1)
+print(colorful.red("Starting bot in 4 seconds!"))
+time.sleep(1)
+print(colorful.orange("Starting bot in 3 seconds!"))
+time.sleep(1)
+print(colorful.yellow("Starting bot in 2 seconds!"))
+time.sleep(1)
+print(colorful.green("Starting bot in 1 seconds!"))
+time.sleep(1)
+clear()
+
 while True:
 	requests.post(url, allow_redirects=False, data={
-	'options': OPTION HERE (check in F12)
+	'options': opid
 	})
 
-	print("Sent a reqest to %s " % (url))
+	print(colorful.green("Sent multiple request to %s " % (url)))
